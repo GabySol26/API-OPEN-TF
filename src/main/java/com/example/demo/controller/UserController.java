f52af98edd1bfde1a328ef784a80329540c94062
Gabriela Nomberto Ramos <102630015+GabySol26@users.noreply.github.com>
2023-06-28 21:41:26 -0500

    Update UserController.java

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/user")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class UserController {
     private final UserService userService;
 
