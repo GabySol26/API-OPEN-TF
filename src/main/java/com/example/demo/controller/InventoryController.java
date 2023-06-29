3118e1d273175d91bfbd708bde9ee73178d16eb9
Gabriela Nomberto Ramos <102630015+GabySol26@users.noreply.github.com>
2023-06-28 21:40:43 -0500

    Update InventoryController.java

--- src/main/java/com/example/demo/controller/InventoryController.java
+++ src/main/java/com/example/demo/controller/InventoryController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/inventory")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class InventoryController {
     private final InventoryService inventoryService;
 
