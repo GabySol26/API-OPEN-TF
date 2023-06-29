d44f72369b170d812ccb5334c485ae58b7c4f9af
Gabriela Nomberto Ramos <102630015+GabySol26@users.noreply.github.com>
2023-06-28 21:41:35 -0500

    Update UserPlanController.java

--- src/main/java/com/example/demo/controller/UserPlanController.java
+++ src/main/java/com/example/demo/controller/UserPlanController.java
@@ -11,7 +11,7 @@ import java.util.Optional;
 
 @RestController
 @RequestMapping("api/v1/userPlan")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*")
 public class UserPlanController {
     private final UserPlanService userPlanService;
 
